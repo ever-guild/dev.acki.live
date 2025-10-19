ef7fdef2ae015e946cfc5b7203fe197ee3f1c348
vcvetkovs <q@qm.local>
2025-10-19 04:17:54 +0300

    Tvm SDK initialization

---
+++ src/hooks.client.ts
@@ -0,0 +1,6 @@
+import { TvmClient } from "@tvmsdk/core";
+// import { libWeb } from "@tvmsdk/lib-web/index.js";
+import { libWeb } from "./tvm_lib.js";
+
+// TvmClient.useBinaryLibrary(libWeb);
+console.log("TVM Client configured with libWeb", TvmClient, libWeb);//, libWeb);
